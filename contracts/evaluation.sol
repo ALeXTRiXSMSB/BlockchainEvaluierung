8c9602093e1726442b40593595eff1a9dd67b1ab
cf_schulze <cf_schulze@web.de>
2019-06-25 14:34:26 +0200

    test

--- contracts/evaluation.sol
+++ contracts/evaluation.sol
@@ -1,21 +1,72 @@
 pragma solidity ^0.4.25;
-pragma experimental ABIEncoderV2;
 
 contract evaluation {
     address public admin;
+    //Studenten adresse (neu)
     address[] public students;
-    faecher faecherStruct;
-    alle_Bewertungen_mit_Fragen alle_Bewertungen_mit_Fragen_Struct;
-    alle_Bewertungsboegen_fuer_bestimmte_Faecher alle_Bewertungsboegen_fuer_bestimmte_Faecher_Struct;
+    Fragebogen[] fragebogen;
+
+    //Wie ist das so?
+    fragebogen.frage_fuenfer[0].frage_f = "TestFrage?";
+
 
     // Faecher
-    struct faecher {
+    struct Fach {
         string name;
+        string dozent_name;
         int anzahl_teilnehmer;
     }
 
-    function setNameFaecher(string newName, int anzahl_teilnehmer) public {
+    struct Frage_fuenfer {
+        string frage_f;
+        int punkte_f;
+    }
+    struct Frage_dreier{
+        string frage_d;
+        int punkte_d;
+    }
+    struct Frage_bool{
+        string frage_b;
+        int punkte_b;
+    }
+    struct Anmerkung{
+        string frage_a;
+        string text;
+    }
+    // Bewertungsbogen
+    struct Fragebogen {
+       //1 Fach
+       Fach fach;
+       // eine Frage mit drei anwortmöglichkeiten
+       Frage_dreier frage_dreier;
+       // eine Anmerkung
+       Anmerkung anmerkung;
+
+        // eine Frage mit Ja/nein
+       Frage_bool frage_bool;
+
+       // dreiundzwanzig Fragen mit fünf antworten
+       Frage_fuenfer[23] frage_fuenfer;
+
+    }
+
+    constructor()public{
+        admin = msg.sender 
+    }
+
+    function setFragebogen(String namefach, String name_dozent, int anzahl_teilnehmer){
         require(msg.sender == admin, "Kann nur der Lehrer festlegen.");
+ 
+    }
+
+
+
+
+
+    function setNameFaecher(string newName) public {
+        require(msg.sender == admin, "Kann nur der Lehrer festlegen.");
+        
+        faecherStruct.name = newName;
     }
 
     function getNameFaecher() public view returns (string) {
@@ -31,64 +82,46 @@ contract evaluation {
         return faecherStruct.anzahl_teilnehmer;
     }
 
-    // Bewertungsbogen
-    struct alle_Bewertungen_mit_Fragen {
-        string str_frage;
-        int int_bewertungsnummer;
-    }
+    
 
     // hier Fragen festlegen
     function setFrage(string newFrage) public {
         require(msg.sender == admin, "Kann nur der Lehrer festlegen.");
-        alle_Bewertungen_mit_Fragen_Struct.str_frage = newFrage;
+        alle_Bewertungen_mit_FragenStruct.str_frage = newFrage;
     }
 
     function getFrage() public view returns (string) {
-        return alle_Bewertungen_mit_Fragen_Struct.str_frage;
+        return alle_Bewertungen_mit_FragenStruct.str_frage;
     }
 
     function setBewertungsnummer(int newBewertungsnummer) public {
         require(newBewertungsnummer >= 1 && newBewertungsnummer <= 5, "Bewertung von 1 bis 5.");
-            alle_Bewertungen_mit_Fragen_Struct.int_bewertungsnummer = newBewertungsnummer;
+            alle_Bewertungen_mit_FragenStruct.int_bewertungsnummer = newBewertungsnummer;
    }
 
     function getBewertungsnummer() public view returns (int) {
-        return alle_Bewertungen_mit_Fragen_Struct.int_bewertungsnummer;
+        return alle_Bewertungen_mit_FragenStruct.int_bewertungsnummer;
    }
 
-    // Bewertungsbogen für Faecher
-    struct alle_Bewertungsboegen_fuer_bestimmte_Faecher {
-        // Anzahl der gesetzten Faecher
-        faecher[5] arr_faecher;
-        // Anzahl der gesetzten Teilnehmer
-        alle_Bewertungen_mit_Fragen[10] arr_bewertungsbogen;
-    }
+ 
 
     // hier Fächer festlegen
-    function setFach(string newFach) public {
-        alle_Bewertungsboegen_fuer_bestimmte_Faecher_Struct.arr_faecher = newFach;
+    function setFach(faecher[] newFach) public {
+        alle_Bewertungsboegen_fuer_bestimmte_FaecherStruct.arr_faecher = newFach;
     }
 
     function getFach() public view returns (string) {
-        return alle_Bewertungsboegen_fuer_bestimmte_Faecher_Struct.arr_faecher;
+        return alle_Bewertungsboegen_fuer_bestimmte_FaecherStruct.arr_faecher;
     }
 
-    function setBewertungen(alle_Bewertungen_mit_Fragen[] newBewertungen) public {
-        alle_Bewertungsboegen_fuer_bestimmte_Faecher_Struct.arr_bewertungsbogen = newBewertungen;
+    function setBewertungen(alle_Bewertungen_mit_Fragen[10] newBewertungen) public {
+        alle_Bewertungsboegen_fuer_bestimmte_FaecherStruct.arr_bewertungsbogen = newBewertungen;
    }
 
-    function getBewertungen() public view returns (alle_Bewertungen_mit_Fragen[]) {
-        return alle_Bewertungsboegen_fuer_bestimmte_Faecher.arr_bewertungsbogen;
+    function getBewertungen() public view returns (alle_Bewertungen_mit_Fragen[10]) {
+        return alle_Bewertungsboegen_fuer_bestimmte_FaecherStruct.arr_bewertungsbogen;
    }
 
-    // für Lehrer und Studenten
-    constructor(string initialNameFaecher, int initialAnzahlTeilnehmer, string initialFrage, int initialBewertungsnummer) public {
-        require(initialBewertungsnummer >= 1 && initialBewertungsnummer <= 5, "Bewertung von 1 bis 5.");
-        alle_Bewertungen_mit_Fragen.int_bewertungsnummer = initialBewertungsnummer;
-        require(msg.sender == admin, "Kann nur der Lehrer festlegen.");
-        faecherStruct.name = initialNameFaecher;
-        faecherStruct.anzahl_teilnehmer = initialAnzahlTeilnehmer;
-        alle_Bewertungsboegen_fuer_bestimmte_Faecher_Struct.str_frage = initialFrage;
-    }
+
 
 }
